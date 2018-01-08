c57deb5475d0049812b482d95e2e4b72d3ad6762
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-01-08 11:13:28 +0000

    Create yes.c

---
+++ yes.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    char a[5]={'y','e','s'};
+    char b[5]={'n','o'};
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        printf("%s",yes);
+    }
+    else
+    {
+        printf("%s",no);
+    }
+    }
+    
