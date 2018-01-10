f803c1fb0fba8dc76494ed7bfe93983aae421a4c
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-01-10 11:02:45 +0000

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a=10;
+    int b=20;
+    int c=30;
+    if(a<b)
+    {
+        printf("%d",a);
+    }
+    if(b<c)
+    {
+        printf("%d",c);
+    }
+}
