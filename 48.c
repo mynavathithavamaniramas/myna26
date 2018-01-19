50df87adadf4c8989634c6a70e48c36dce1343cb
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-01-19 10:31:16 +0000

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i;
+    printf("enter the i value");
+    scanf("%d",&i);
+    for(i=0;i>3;i++)
+    {
+        printf("%d",&i);
+    }
+}
+
+
+
