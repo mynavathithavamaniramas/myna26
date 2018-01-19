84ca116e5b17b126a547378e4a6b50cea77f4d41
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-01-19 11:06:45 +0000

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+    int i,num=0;
+    printf("enter the i value");
+    scanf("%d",&i);
+    while(num!=0)
+    {
+        num=i+10;
+        printf("%d",num);
+        }
+switch(i)
+{
+    case 1:
+    printf("one");
+    break;
+    case 2:
+    printf("two");
+    break;
+    case 3:
+    printf("three");
+    break;
+}
+}
