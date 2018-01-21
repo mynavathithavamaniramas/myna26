dd34b54d179aedc571fa0638d01ea8ea7151cfae
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-01-21 14:38:18 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a=9,b=3,c;
+    c=a*b;
+    if(c%2==0)
+    {
+        printf("the number is even");
+    }
+else
+{
+    printf("the number is odd");
+}
+}
