bb2a204bcfeb424b4dab919b4fa19959eb0248e2
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-01-24 16:29:36 +0530

    Create 60.c

---
+++ 60.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int t=0,c=1,i;
+    for(i=0;i<=5;++i)
+    {
+        printf("%d",t);
+        s=t+c;
+        t=c;
+        c=s;
+        
+        
+    }
+}
