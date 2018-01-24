5563af324ff3f61a5233a51d90a08ef1b0b92a61
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-01-24 16:19:42 +0530

    Rename 59.c to 590.c

---
+++ 590.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter the a value");
+    scanf("%d",&a);
+    for(a=1;a>10;a++)
+    {
+      printf("the a value is%d",a); 
+    }
+else
+{
+    printf("its is the smaller value");
+}
+}
+
+
