18d1c7dc63263469293d408d579f2c45ab5fe76e
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-01-31 16:47:01 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a=68,b=23,c;
+    c=a+b;
+    if(c%2==0)
+    {
+        printf("given number is even");
+    }
+    else
+    {
+        printf("odd number");
+    }
+    }
