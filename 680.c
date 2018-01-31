74f17a0734b9e4de4f7d6200467c8d864d000a29
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-01-31 16:00:54 +0530

    Create 680.c

---
+++ 680.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the value");
+    scanf("%d",&a);
+    if(a%7==0)
+    {
+        printf("yes");
+    }
+
+    else
+    {
+        printf("no");
+    }
+}
+
+
