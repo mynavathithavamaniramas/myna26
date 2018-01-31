64b0fc5cf2593b6fbe2d4147ebdafbca0e5f7c2d
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-01-31 16:37:46 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the a value");
+    scanf("%d",&a);    
+        if(a%2==1)
+    {
+        printf("it is prime");
+    }
+    else
+    {
+        printf("not a prime");
+    }
+        }
+    
