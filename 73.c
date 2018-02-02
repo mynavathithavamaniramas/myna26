b890b5a7dfd4d548c9aeb9c9ea3a19dd39f8b5fe
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-02-02 14:56:44 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=2,b=5,c;
+    printf("enter the c value");
+    scanf("%d",&c);
+    if(a>b)
+    {
+        if(a>c)
+        printf("yes");
+    }
+    if(a<c)
+        printf("no");
+    }
+
