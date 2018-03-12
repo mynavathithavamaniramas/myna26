7368ec76f10d0ea8c4ceed0df2ca237cacfa5cb9
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-03-12 16:28:35 +0530

    Create 80.c

---
+++ 80.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i;
+printf("enter the array element");
+for(i=0;i<4;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<4;i++)
+{
+if(a[i]%2==1)
+{
+printf("%d",i);
+}
+}
+}
