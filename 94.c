e6b73637524fb694b6ef823f3d6c9a33443e6bab
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-03-14 16:55:36 +0530

    Create 94.c

---
+++ 94.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],n=3,k=4,i;
+    printf("enter the array value");
+    for(i=0;i<6;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<6;i++)
+    {
+        if(i==n)
+        printf("%d",n);
+        
+}
+}
